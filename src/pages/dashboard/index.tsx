a9312e7aae97a989ccf67ece17925fff0153acb9
rafalmeidas <rafalmeidas21@gmail.com>
2023-08-23 08:43:32 -0300

    tela inicial e login com Google funcionais

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+
+import styles from "./styles.module.css";
+
+export default function Dashboard() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Meu painel de tarefas</title>
+      </Head>
+
+      <h1>Página Painel</h1>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
